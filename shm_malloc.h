6b0894c1aafc6f70982d55a366a71a17e65444d2
AmirHossein Seyri <31968924+AAMH@users.noreply.github.com>
2018-03-02 11:22:24 -0600

    Update shm_malloc.h

--- shm_malloc.h
+++ shm_malloc.h
@@ -1,6 +1,9 @@
 struct tracker {        /* Shared memory structure to keep track of current pointer */
     size_t max_size;
     off_t allocated_size;
+    void * start_address;
+    void * last_address;
 };
 
-extern void* shm_malloc(size_t n);
+extern struct tracker shm_malloc(size_t n);
+extern struct tracker get_tracker(void);
